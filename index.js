eaa4da7f1cd4b5df996ec323cf04e744ba33f4e7
Massimilianok <massimiliano.rizzuto87@gmail.com>
2021-03-04 14:13:00 +0100

    feat: Add more security

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
 const repos = require('./router/repos');
 const email = require('./router/email');
 
+const corsOptions = {
+  origin: 'https://massimilianorizzuto.dev',
+  optionsSuccessStatus: 200,
+};
+
 const app = express();
+
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
+app.use(helmet());
 app.use('/api/repos', repos);
 app.use('/api/email', email);
 
